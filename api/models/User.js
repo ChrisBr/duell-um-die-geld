37ae0d451c332da5c50769405aed230b1bffbf05
Christian Bruckmayer <cbruckmayer@suse.com>
2017-02-22 10:33:03 +0100

    Add user api

---
+++ api/models/User.js
@@ -0,0 +1,24 @@
+/**
+ * User.js
+ *
+ * @description :: TODO: You might write a short summary of how this model works and what it represents here.
+ * @docs        :: http://sailsjs.org/documentation/concepts/models-and-orm/models
+ */
+
+module.exports = {
+
+  attributes: {
+    name: {
+      type: 'string',
+      defaultsTo: ''
+    },
+    account: {
+      type: 'integer',
+      defaultsTo: 500
+    },
+    answer: {
+      type: 'string',
+      defaultsTo: ''
+    }
+  }
+};
